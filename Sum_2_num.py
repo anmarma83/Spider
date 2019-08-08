b0222b1777a6118277a1ff78018eb70dfcfdf53f
anmarma83 <anmarma83@gmail.com>
2019-08-08 22:02:57 +0300

    Third updating

--- Sum_2_num.py
+++ Sum_2_num.py
@@ -1,5 +1,5 @@
 n1 =  int(input("Enter First number"))
-n2 =  int(input("Enter First number"))
-print("The summation of two numbers is:",(n1+))
+n2 =  int(input("Enter Second number"))
+print("The summation of two numbers is:",(n1+n2))
 print('just for checking updating')
 
---
+++ multiplication.py
@@ -0,0 +1,5 @@
+n1 =  int(input("Enter First number"))
+n2 =  int(input("Enter Second number"))
+print("The multiplication of two numbers is:",(n1*n2))
+
+
